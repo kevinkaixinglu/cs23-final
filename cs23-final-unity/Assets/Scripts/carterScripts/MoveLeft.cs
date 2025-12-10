840bb2ff1eeb6fddb8103d69ca49bd3fd223c899
Carter-C8 <colusacarter@gmail.com>
2025-12-09 20:53:49 -0500

    added art to level

--- cs23-final-unity/Assets/Scripts/carterScripts/MoveLeft.cs
+++ cs23-final-unity/Assets/Scripts/carterScripts/MoveLeft.cs
@@ -3,15 +3,22 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     public float speed = 5f;
+    public float pulseSpeed = 0.5f;     // how fast it pulses
+    public float pulseAmount = 0.2f;  // how much bigger/smaller it gets
+    private Vector3 originalScale;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        originalScale = transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //pulse
+        float scale = 1f + Mathf.Sin(Time.time * pulseSpeed) * pulseAmount;
+        transform.localScale = originalScale * scale;
+
         transform.Translate(Vector2.left * speed * Time.deltaTime);
         if (transform.position.x < -15f) {
             Destroy(gameObject);
--- cs23-final-unity/Assets/Scripts/carterScripts/carterbeatmap.cs
+++ cs23-final-unity/Assets/Scripts/carterScripts/carterbeatmap.cs
@@ -13,13 +13,13 @@ public class carterbeatmap : MakeBeatmap
         beatmapBuilder builder = new beatmapBuilder(num_measures);
         int meas;
 
-        meas = 9;
-        builder.PlaceSixteenthNote(meas, 1, 2, -1); // WINE
+        meas = 3;
+        builder.PlaceSixteenthNote(meas, 1, 2, 1); // WINE
         builder.PlaceSixteenthNote(meas, 1, 1, 1);
         builder.PlaceSixteenthNote(meas, 1, 3, 1);
-        builder.PlaceSixteenthNote(meas, 2, 1, 1);
-        builder.PlaceSixteenthNote(meas, 3, 1, 1);
-        builder.PlaceSixteenthNote(meas, 4, 3, 1);
+        builder.PlaceSixteenthNote(meas, 2, 1, 2);
+        builder.PlaceSixteenthNote(meas, 3, 1, 2);
+        builder.PlaceSixteenthNote(meas, 4, 3, 3);
         meas += 2;
         builder.PlaceSixteenthNote(meas, 1, 2, -1); // WINE
         builder.PlaceSixteenthNote(meas, 1, 1, 1);
